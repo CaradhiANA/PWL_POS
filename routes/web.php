699ff88d26fabc5b47d0f0128024f33bb288d7d0
caradhiANA <aleganaufal@example.com>
2024-02-29 21:53:03 +0700

    Implementasi DB Facade (Pratikum 4)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Levelcontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/level', [Levelcontroller::class, 'index']);
